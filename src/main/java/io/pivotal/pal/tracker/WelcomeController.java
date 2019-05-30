44f0c476cb8176f35a311bb4b2f76d8ab237dbed
Anthony Ikeda <anthony.ikeda@cognizant.com>
2019-05-29 18:23:52 -0700

    Latest workshop

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,5 +1,7 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+    private String message;
+
+    @Autowired
+    public WelcomeController(@Value("${welcome.message}")String message) {
+        this.message = message;
+    }
+
     @GetMapping("/")
     public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok().body("hello");
+        return ResponseEntity.ok().body(this.message);
     }
 }
--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -2,15 +2,18 @@ package test.pivotal.pal.tracker;
 
 import io.pivotal.pal.tracker.WelcomeController;
 import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 public class WelcomeControllerTest {
+
 
     @Test
     public void itSaysHello() throws Exception {
         WelcomeController controller = new WelcomeController("A welcome message");
 
-        assertThat(controller.sayHello()).isEqualTo("A welcome message");
+        assertThat(controller.sayHello().getBody()).isEqualTo("A welcome message");
     }
 }
